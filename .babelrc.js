8ab7f1ae50c949285f784ac714e9196254d587a3
HoseaLe <hoseale@163.com>
2020-02-29 22:17:46 +0800

    修改

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "presets": [
     [
       "@babel/preset-env", { 
-        "modules": 'umd', // 不进行model转换， 默认为auto
+        "modules": false, // 不进行model转换， 默认为auto
       }
     ]
   ],
