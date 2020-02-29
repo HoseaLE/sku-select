62d437e9b5c43c440af95d124fd2f8336b9d247f
HoseaLe <hoseale@163.com>
2020-02-29 23:19:33 +0800

    修改bug

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sku-select
+# sku-calculate
 sku属性计算，输入选中属性，返回属性可选状态
 
 ### 安装
--- dll/index.js
+++ dll/index.js
@@ -1,3 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -139,4 +146,5 @@ var Sku = /*#__PURE__*/function () {
   return Sku;
 }();
 
-export default Sku;
+var _default = Sku;
+exports["default"] = _default;
